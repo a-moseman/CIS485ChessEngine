46840a3bf63e15d8367f44dd6c7a578c00d49d32
a-moseman <drewmoseman@gmail.com>
2023-02-12 18:38:46 -0500

    Implement basic version of MCTS.

--- src/main/java/cis485/chessengine/Engine.java
+++ src/main/java/cis485/chessengine/Engine.java
@@ -4,12 +4,13 @@ import com.github.bhlangonijr.chesslib.Board;
 import com.github.bhlangonijr.chesslib.Side;
 import com.github.bhlangonijr.chesslib.move.Move;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Engine {
     private Random random;
-    private int secondsPerMove = 10;
+    private int secondsPerMove = 2;
     private Board board;
     private Side side;
 
@@ -20,30 +21,177 @@ public class Engine {
 
     //https://www.analyticsvidhya.com/blog/2019/01/monte-carlo-tree-search-introduction-algorithm-deepmind-alphago/
 
-    public void run(String position) {
+    class Node {
+        Node parent;
+        List<Node> children;
+        Board position;
+        Move move;
+        int totalSimReward;
+        int totalVisits;
+        boolean visited;
+
+        public Node(Move move, Board position, Node parent) {
+            this.move = move;
+            this.position = position;
+            this.parent = parent;
+            this.children = new ArrayList<>();
+        }
+    }
+
+    Node root;
+
+    //https://int8.io/monte-carlo-tree-search-beginners-guide/
+
+    public Move run(String position) {
         board.loadFromFen(position);
         side = board.getSideToMove();
-        // todo
+        root = new Node(null, board, null);
+        visit(root);
+        return mcts(root);
     }
 
-    private int rollout(Board board) {
-        if (board.isMated()) {
-            return board.getSideToMove() == side ? -1 : 1;
+    private void visit(Node node) {
+        node.visited = true;
+        List<Move> legalMoves = node.position.legalMoves();
+        for (Move move : legalMoves) {
+            Board newBoard = node.position.clone();
+            newBoard.doMove(move);
+            Node child = new Node(move, newBoard, node);
+            node.children.add(child);
         }
-        if (board.isDraw()) {
-            return 0;
-        }
-
-        List<Move> moves = board.legalMoves();
-        Move move = moves.get(random.nextInt(moves.size() - 1));
-        board.doMove(move);
-        int score = rollout(board);
-        board.undoMove();
-        return score;
     }
 
-    private int evaluate(Board board) {
-        return 0; // todo: apply ml
+    private Move mcts(Node root) {
+        long startTime = System.nanoTime();
+        while (System.nanoTime() - startTime < 1_000_000_000L * secondsPerMove) {
+            Node leaf = traverse(root);
+            int simResult = rollout(leaf);
+            //System.out.println(leaf.move + " - " + simResult); // DEBUG
+            backpropagate(leaf, simResult);
+        }
+        return bestChild(root).move;
+    }
+
+    private Node traverse(Node node) {
+        while (isFullyExpanded(node)) {
+            node = bestUCT(node);
+        }
+        return pickUnvisited(node.children);
+    }
+
+    private int rollout(Node node) {
+        // DONE
+        while (isNonTerminal(node)) {
+            visit(node);
+            node = rollOutPolicy(node);
+        }
+        return result(node);
+    }
+
+    private void backpropagate(Node node, int result) {
+        // DONE
+        if (root.equals(node)) {
+            return;
+        }
+        node.totalVisits++;
+        node.totalSimReward += result;
+        backpropagate(node.parent, result);
+    }
+
+    private Node rollOutPolicy(Node node) {
+        // TODO: impelement ml stuff?
+        return pickRandom(node.children);
+        /*
+        int bestScore = Evaluator.materialBalance(node.children.get(0).position, side);
+        int best = 0;
+        for (int i = 0; i < node.children.size(); i++) {
+            int score = Evaluator.materialBalance(node.children.get(i).position, side);
+            if (bestScore < score) {
+                bestScore = score;
+                best = i;
+            }
+        }
+        return node.children.get(best);
+         */
+    }
+
+    private boolean isNonTerminal(Node node) {
+        // DONE
+        return !node.position.isMated() && !node.position.isDraw();
+    }
+
+    private boolean isFullyExpanded(Node node) {
+        // DONE
+        for (Node child : node.children) {
+            if (!child.visited) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Node bestUCT(Node node) {
+        // DONE
+        double bestUct = uctOfChild(node.children.get(0), node);
+        int best = 0;
+        for (int i = 0; i < node.children.size(); i++) {
+            double uct = uctOfChild(node.children.get(i), node);
+            if (bestUct < uct) {
+                bestUct = uct;
+                best = i;
+            }
+        }
+        return node.children.get(best);
+    }
+
+    private double uctOfChild(Node child, Node parent) {
+        // DONE
+        double c = 0.5;
+        double exploitationComponent = (double) child.totalSimReward / child.totalVisits;
+        double explorationComponent = Math.sqrt(Math.log(parent.totalVisits) / child.totalVisits);
+        return exploitationComponent + c * explorationComponent;
+    }
+
+    private Node pickUnvisited(List<Node> children) {
+        // DONE
+        for (Node child : children) {
+            if (!child.visited) {
+                return child;
+            }
+        }
+        return null;
+    }
+
+    private Node pickRandom(List<Node> children) {
+        // DONE
+        int choice = random.nextInt(children.size());
+        return children.get(choice);
+    }
+
+    private Node bestChild(Node node) {
+        // DONE
+        int best = 0;
+        for (int i = 1; i < node.children.size(); i++) {
+            if (node.children.get(best).totalSimReward < node.children.get(i).totalSimReward) {
+                best = i;
+            }
+        }
+        return node.children.get(best);
+    }
+
+    private int result(Node node) {
+        if (node.position.isMated()) {
+            if (node.position.getSideToMove() != side) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        }
+        if (node.position.isDraw()) {
+            return -1;
+        }
+        return 0; // should never happen
     }
 
     public int getSecondsPerMove() {
--- src/main/java/cis485/chessengine/Main.java
+++ src/main/java/cis485/chessengine/Main.java
@@ -1,7 +1,21 @@
 package cis485.chessengine;
 
+import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.Side;
+
 public class Main {
     public static void main(String[] args) {
-        Engine engine = new Engine();
+        Engine a = new Engine();
+        Engine b = new Engine();
+        Board board = new Board();
+        while (!board.isDraw() && !board.isMated()) {
+            System.out.println(board);
+            if (board.getSideToMove() == Side.WHITE) {
+                board.doMove(a.run(board.getFen()));
+            }
+            else {
+                board.doMove(b.run(board.getFen()));
+            }
+        }
     }
 }
