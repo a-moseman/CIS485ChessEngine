fc7e36145346655c9df43f5ecf82e5b56d54478b
a-moseman <drewmoseman@gmail.com>
2023-03-03 03:42:33 -0500

    Begin implementation of transposition table.

--- src/main/java/cis485/chessengine/Engine/Search/MCTS.java
+++ src/main/java/cis485/chessengine/Engine/Search/MCTS.java
@@ -15,32 +15,21 @@ public class MCTS {
     private Side side;
     private Node root;
     private int visits;
+    private HashMap<Long, Node> transpositionTable;
 
     public MCTS(MultiLayerNetwork model) {
         this.MODEL = model;
-    }
-
-    private void add(Node node) {
-        List<Move> legalMoves = node.position.legalMoves();
-        node.children = new Node[legalMoves.size()];
-        for (int i = 0; i < node.children.length; i++) {
-            Move move = legalMoves.get(i);
-            Board newBoard = new Board();
-            newBoard.loadFromFen(node.position.getFen()); // faster than Board.clone()
-            newBoard.doMove(move);
-            Node child = new Node(move, newBoard, node);
-            node.children[i] = child;
-        }
+        this.transpositionTable = new HashMap<>();
     }
 
     public void initialize(Side side, String position) {
+        transpositionTable.clear();
         this.side = side;
         Board board = new Board();
         board.loadFromFen(position);
-        root = new Node(null, board, null);
+        root = new Node(null, board);
         root.visited = true;
         root.totalVisits = 1;
-        add(root);
         visits = 0;
     }
 
@@ -98,17 +87,13 @@ public class MCTS {
 
     private Node traverse(Node node) {
         while (isFullyExpanded(node)) {
-            node = bestUCT(node);
-            add(node);
+            node = bestUCT(node); // can only be a visited node
         }
-        if (node.children.length == 0) {
-            return node;
-        }
-        return pickUnvisited(node.children);
+        return pickUnvisited(node);
     }
 
     private void backpropagate(Node node, Prediction prediction) {
-        visits++;
+        //visits++;
         node.visited = true;
 
         node.totalVisits++;
@@ -126,57 +111,69 @@ public class MCTS {
         if (root.equals(node)) {
             return;
         }
-
-        backpropagate(node.parent, prediction);
+        for (Node parent : node.parents) {
+            backpropagate(parent, prediction);
+        }
     }
 
     private boolean isFullyExpanded(Node node) {
-        if (node.children.length == 0) {
+        if (node.children.size() < node.position.legalMoves().size()) {
             return false;
-        }
-        for (Node child : node.children) {
-            if (!child.visited) {
-                return false;
-            }
         }
         return true;
     }
 
     private Node bestUCT(Node node) {
         int best = 0;
-        for (int i = 1; i < node.children.length; i++) {
-            if (uctOfChild(node.children[best]) < uctOfChild(node.children[i])) {
+        for (int i = 1; i < node.children.size(); i++) {
+            if (uctOfChild(node.children.get(best)) < uctOfChild(node.children.get(i))) {
                 best = i;
             }
         }
-        return node.children[best];
+        return node.children.get(best);
     }
 
     private double uctOfChild(Node child) {
         double c = Math.sqrt(2);
         double exploitationComponent = (double) child.getTotalSimReward(side) / child.totalVisits;
-        double explorationComponent = Math.sqrt(Math.log(child.parent.totalVisits) / child.totalVisits);
+        double explorationComponent = Math.sqrt(Math.log(child.getTotalParentVisits()) / child.totalVisits);
         return exploitationComponent + c * explorationComponent;
     }
 
-    private Node pickUnvisited(Node[] children) {
-        for (Node child : children) {
-            if (!child.visited) {
-                return child;
-            }
+    private Node pickUnvisited(Node node) {
+        List<Move> legalMoves = node.position.legalMoves();
+        if (legalMoves.size() == 0) { // node is terminal
+            return node;
         }
-        return null;
+        // add unvisited child to tree
+        Board childBoard = new Board();
+        childBoard.loadFromFen(node.position.getFen());
+        Move move = legalMoves.get(node.children.size());
+        childBoard.doMove(move);
+        // check if transposition
+        Node child;
+        if (transpositionTable.containsKey(childBoard.getZobristKey())) {
+            child = transpositionTable.get(childBoard.getZobristKey());
+        }
+        else {
+            visits++; // should only be unique positions
+            child = new Node(move, childBoard);
+
+        }
+        node.children.add(child);
+        child.parents.add(node);
+        return child;
     }
 
     public Move getBest() {
         int best = 0;
-        for (int i = 1; i < root.children.length; i++) {
-            if (root.children[best].getTotalSimReward(side) < root.children[i].getTotalSimReward(side)) {
+        for (int i = 1; i < root.children.size(); i++) {
+            if (root.children.get(best).getTotalSimReward(side) < root.children.get(i).getTotalSimReward(side)) {
             //if (root.children[best].totalVisits < root.children[i].totalVisits) {
                 best = i;
             }
         }
-        return root.children[best].move;
+        return root.children.get(best).move;
     }
 
     public int getVisits() {
